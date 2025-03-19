9d51b6c815f99e4581029c69df043b0ec6d6ae00
Jonathan Bourland <jonathanbourland@hotmail.com>
2025-03-19 17:38:35 -0400

    Test Composer events by adding to activate() and adding onPrePackageInstall event

--- src/MoodleInstallerPlugin.php
+++ src/MoodleInstallerPlugin.php
@@ -4,12 +4,16 @@ namespace Middag;
 
 use Composer\Composer;
 use Composer\IO\IOInterface;
+use Composer\Installer\PackageEvent;
+use Composer\Installer\PackageEvents;
 use Composer\Plugin\PluginInterface;
+use cweagans\Composer\PatchEvent;
 
 class MoodleInstallerPlugin implements PluginInterface {
     public function activate(Composer $composer, IOInterface $io)
     {
-
+        $this->composer = $composer;
+        $this->io = $io;
     }
 
     public function deactivate(Composer $composer, IOInterface $io)
@@ -25,6 +29,7 @@ class MoodleInstallerPlugin implements PluginInterface {
     public function getSubscribedEvents(): array
     {
         return array(
+            PackageEvents::PRE_PACKAGE_INSTALL => ['onPrePackageInstall'],
             PatchEvents::POST_PATCH_APPLY => ['copyPatch'],
         );
     }
@@ -35,5 +40,10 @@ class MoodleInstallerPlugin implements PluginInterface {
         $target = $event->getTarget();
         $this->io->write("Copying patch {$patch->getPatch()} to {$target}");
         copy($patch->getPatch(), $target);
+    }
+
+    public function onPrePackageInstall(PackageEvent $event): void
+    {
+        $test = $event;
     }
 }
